25cd2b8cde9309b8dab3100b7b65e76058211ce7
Alok Kumar Singh <86158900+RanajiAlok@users.noreply.github.com>
2022-12-31 18:06:05 +0530

    Update subsetsleetcode.cpp

--- subsetsleetcode.cpp
+++ subsetsleetcode.cpp
@@ -17,3 +17,30 @@ public:
         return ans;
     }
 };
+
+/*Given an integer array nums that may contain duplicates, return all possible
+subsets
+(the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order.*/
+
+class Solution {
+public:
+    void sol(int ind,vector<int>& arr,vector<vector<int>>& ans,vector<int>& ds){
+        ans.push_back(ds);
+
+        for(int i=ind;i<arr.size();i++){
+            if(i!=ind && arr[i]==arr[i-1])continue;
+            ds.push_back(arr[i]);
+            sol(i+1,arr,ans,ds);
+            ds.pop_back();
+        }
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        sort(nums.begin(),nums.end());
+        sol(0,nums,ans,ds);
+        return ans;
+    }
+};
