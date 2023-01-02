5c23627a2cd999e70c41b85ca329fdc594385e92
Alok Kumar Singh <86158900+RanajiAlok@users.noreply.github.com>
2023-01-02 19:40:04 +0530

    Update permutationleetcode.cpp

--- permutationleetcode.cpp
+++ permutationleetcode.cpp
@@ -24,3 +24,25 @@ public:
         return ans;
     }
 };
+
+//Alternate solution with less space complexity
+class Solution {
+public:
+    void permutation(vector<int>& nums,vector<vector<int>>& ans,int ind){
+        if(ind==nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+
+        for(int i=ind;i<nums.size();i++){
+            swap(nums[ind],nums[i]);
+            permutation(nums,ans,ind+1);
+            swap(nums[ind],nums[i]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        permutation(nums,ans,0);
+        return ans;
+    }
+};
